6d58ea895d7f2111cb45c53cd4849178013be95e
Viacheslav Golyshevskii <python_poseur@MacBook-Air-Viacheslav.local>
2022-12-24 17:33:28 +0400

    orders_detail.idx row number settings

--- O_OD_TRIGGERS_W1251.sql
+++ O_OD_TRIGGERS_W1251.sql
@@ -1,12 +1,15 @@
-/* 1.
+/* 1. 5.
 тригер на обновление значений атрибута str_sum таблицы orders_detail
 после обновления значений атрибута discount таблицы orders
 */
 create or replace trigger od_strsum_by_discount_trigger
 after update discount on orders
-declare
-    pragma exception_init()
 begin
+    -- обработка исключения если discount не лежит в диапазоне 0 - 100
+    if :new.discount < 0 or :new.discount > 100 then
+        raise_application_error(-20001, 'Скидка не должна превышать значения 100 и быть меньше 0');
+    end if;
+
     -- обновление таблицы orders_detail по атрибуту id_order
     update orders_detail 
     set str_sum = price * qty * (1 - :new.discount / 100)
@@ -49,24 +52,37 @@ begin
 end;
 
 
-/* 3.
+/* 3. 4.
 тригер для обновления значения атрибута str_sum таблицы orders_detail
 в случае обновления значения атрибута price или qty
+и добавления значения в idx в случае вставки строки
 */
 create or replace trigger od_strsum_by_price_qty_trigger
-before update price, qty on orders_detail
+before update price, qty or insert on orders_detail
 declare
     v_discount number;
+    v_idx_max number;
 begin
-    -- выборка скидки из таблицы orders по id
-    select discount into v_discount from orders
-    where id = :new.id_order;
+    -- определение команды тригера
+    if updating('price') or updating('qty') then
+        -- выборка скидки из таблицы orders по id
+        select discount into v_discount from orders
+        where id = :old.id_order;
 
-    -- обновление таблицы orders_detail по id_order
-    update orders_detail
-    set str_sum = :new.price * :new:qty * (1 - v_discount / 100)
-    where id_order = :new.id_order;
+        -- обновление таблицы orders_detail по id_order
+        update orders_detail
+        set str_sum = :new.price * :new:qty * (1 - v_discount / 100)
+        where id_order = :old.id_order;
+    elsif inserting then
+        -- выборка максимального порядкового номера строки относительно номера заказа
+        select max(idx) into v_idx_max from orders_detail
+        where id_order = :new.id_order;
+
+        -- обновление значения idx
+        :new.idx = v_idx_max;
+    end if;
 end;
+
 
 -- активация тригеров для каждой таблицы
 alter table orders enable all triggers;
